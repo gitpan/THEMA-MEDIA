0681b30e6c3bec0e2f95a40c585ff98de3970733
Theo van Hoesel <Th.J.v.Hoesel+CPAN@gmail.com>
2014-10-07 13:20:15 +0000

    Import of VANHOESEL/THEMA-MEDIA-2014.10.02 from CPAN.
    gitpan-cpan-distribution: THEMA-MEDIA
    gitpan-cpan-version:      2014.10.02
    gitpan-cpan-path:         VANHOESEL/THEMA-MEDIA-2014.10.02.tar.gz
    gitpan-cpan-author:       VANHOESEL
    gitpan-cpan-maturity:     released

--- lib/THEMA/MEDIA.pm
+++ lib/THEMA/MEDIA.pm
@@ -1,6 +1,29 @@
 package THEMA::MEDIA;
 
-# ABSTRACT: Much has changed in twenty years... but THEMA-MEDIA still provides solutions based on proven technologies
+our $VERSION = 2014.1002_001;
+
+
+
+                                        
+                    ########            
+                    ########            
+                    ########  ########  
+          ########  ########  ########  
+          ########            ########  
+          ########  ########  ########  
+          ########  ########            
+                    ########  ########  
+          ########  ########  ########  
+          ########            ########  
+          ########  ########  ########  
+          ########  ########            
+                    ########            
+                    ########            (c) 1994  www.THEMA-MEDIA.nl
+                                        
+
+
+
+
 
 print <<BANNER_END;
 
@@ -24,8 +47,6 @@ print <<BANNER_END;
                                  
 BANNER_END
 
-
-
 1;
 
 __END__
@@ -36,11 +57,51 @@ __END__
 
 =head1 NAME
 
-THEMA::MEDIA - Much has changed in twenty years... but THEMA-MEDIA still provides solutions based on proven technologies
+THEMA::MEDIA
 
 =head1 VERSION
 
-version 2014.10.01
+version 2014.10.02
+
+=head1 SYNOPSIS
+
+  #! /usr/bin/env perl
+  
+  use strict;
+  use warnings;
+  
+  use v5.20.1;
+  use feature "signatures";
+  
+  use THEMA::MEDIA;
+
+=head1 DESCRIPTION
+
+... I will come up with a proper description
+
+=head1 NAME
+
+THEMA-MEDIA - Providing solutions with Modern Perl
+
+=head1 ABSTRACT
+
+Much has changed in twenty years...
+But THEMA-MEDIA still provides solutions based on proven technologies
+
+=head1 EXPORT
+
+none, there is no code yet
+
+=head1 AUTHOR
+
+Theo van Hoesel, <Th.J.v.Hoesel@THEMA-MEDIA.nl>
+
+=head1 COPYRIGHT AND LICENSE
+
+(c) 2014 by THEMA-MEDIA, Th. J. van Hoesel
+
+This library is free software; you can redistribute it and/or modify
+it under the same licence as Perl itself.
 
 =head1 AUTHOR
 
